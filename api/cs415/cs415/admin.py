f59a30deefa031b9b9b4bd527a79974943919d14
JNope <91224053+JNope01@users.noreply.github.com>
2024-02-28 14:27:29 -0700

    clean up

--- api/cs415/cs415/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Userdaylog, Usergoals, Userinfo
-
-admin.site.register(User)
-admin.site.register(Userdaylog)
-admin.site.register(Usergoals)
-admin.site.register(Userinfo)
